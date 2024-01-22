2eb7e2a6c14ad74a271f4ce0ce5f35c5c02a8ddf
Garrett SY Hampton G@GarrettHampton.com <g@garretthampton.com>
2024-01-22 11:17:19 -0800

    Updated app

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,26 +1,39 @@
-import './bootstrap.min.css';
-import React from 'react';
-import HomePage from './homepage.js';
-import ContactUs from './contactus.js';
+import "./bootstrap.min.css";
+import React from "react";
+import HomePage from "./homepage.js";
+import ContactUs from "./contactus.js";
 
-/*
-Copy the Web Chat code from Watson Assitant and paste it here
-*/
+window.watsonAssistantChatOptions = {
+  integrationID: "1408a783-f1de-45b6-86bd-ce141cdb5790", // The ID of this integration.
+  region: "us-south", // The region your integration is hosted in.
+  serviceInstanceID: "3644a3e6-236e-410c-9d7c-b61598f55481", // The ID of your service instance.
+  onLoad: async (instance) => {
+    await instance.render();
+  },
+};
+
+setTimeout(function () {
+  const t = document.createElement("script");
+  t.src =
+    "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+    (window.watsonAssistantChatOptions.clientVersion || "latest") +
+    "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
 
 class App extends React.Component {
-
   state = {
-    pageshown:<HomePage/>
-  }
+    pageshown: <HomePage />,
+  };
 
-  setPageHome = ()=> {
-    this.setState({pageshown:<HomePage/>});    
-  }
-  
-  setPageContactUs = ()=> {
-    this.setState({pageshown:<ContactUs/>});    
-  }
-  
+  setPageHome = () => {
+    this.setState({ pageshown: <HomePage /> });
+  };
+
+  setPageContactUs = () => {
+    this.setState({ pageshown: <ContactUs /> });
+  };
+
   render() {
     return (
       <div>
@@ -28,21 +41,29 @@ class App extends React.Component {
           <div class="navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item active">
-                <a class="nav-link" href="#" onClick={this.setPageHome}>Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#" onClick={this.setPageHome}>
+                  Home <span class="sr-only">(current)</span>
+                </a>
               </li>
-                /*
-                Add more tabs and action items for the tabs to give the dealership website a real feel. 
-                */
-        
               <li class="nav-item">
-                <a class="nav-link" href="#" onClick={this.setPageContactUs}>Contact Us</a>
+                <a class="nav-link" href="#" onClick={this.setPageContactUs}>
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" onClick={this.setPageContactUs}>
+                  FAQ
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" onClick={this.setPageContactUs}>
+                  Contact Us
+                </a>
               </li>
             </ul>
           </div>
         </nav>
-        <div>
-          {this.state.pageshown}
-        </div>
+        <div>{this.state.pageshown}</div>
       </div>
     );
   }
--- server/frontend/src/homepage.js
+++ server/frontend/src/homepage.js
@@ -1,20 +1,32 @@
-import './bootstrap.min.css';
-import React from 'react';
+import "./bootstrap.min.css";
+import React from "react";
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <div style={{display:"flex"}}>
-        <img src='/cardealership.jpeg' style={{width: "300px"}}/>
-        <div style={{margin:"100px"}}>
-             <span style={{margin:"60px"}}>
-             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-               </span >
-           </div>
-
+        <div style={{ display: "flex" }}>
+          <img src="/cardealership.jpeg" style={{ width: "300px" }} />
+          <div style={{ margin: "100px" }}>
+            <div className="jumbotron">
+              <h1 className="display-4">Welcome To Our Dealserhip!</h1>
+              <p className="lead">
+                Please feel free to use our chat-bot on the bottom right to
+                schedule appointments or ask questions.
+              </p>
+              <hr className="my-4" />
+              <p>
+                It uses AI from IBM WatsonX to answer your questions and
+                schedule appointments.
+              </p>
+              <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">
+                  Learn more
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-        
       </div>
     );
   }
